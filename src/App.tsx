590ef5bad9b8de40ab8cdcd3ec671ce6e1e2a3ce
Joakamakaka1 <joaquinfutbolero11@gmail.com>
2025-10-22 15:20:58 +0200

    Speed insight

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Routes from "./Routes";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 const App: React.FC = () => {
   return (
     <>
       <Routes />
       <Analytics />
+      <SpeedInsights />
     </>
   );
 };
