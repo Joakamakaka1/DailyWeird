df690d359e86a9189de71044742b96e6b9041420
Joakamakaka1 <joaquinfutbolero11@gmail.com>
2025-10-22 15:01:55 +0200

    Fix .env

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
-import { API_URL } from "../../config";
 
 const api = axios.create({
-  baseURL: API_URL,
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
