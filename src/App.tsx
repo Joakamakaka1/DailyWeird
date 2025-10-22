27daee5ab29a64dc5f310958a13b97cc1e93f35c
Joakamakaka1 <joaquinfutbolero11@gmail.com>
2025-10-22 15:17:54 +0200

    Analytics vercel

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import Routes from "./Routes";
+import { Analytics } from "@vercel/analytics/next";
 
 const App: React.FC = () => {
-  return <Routes />;
+  return (
+    <>
+      <Routes />
+      <Analytics />
+    </>
+  );
 };
 
 export default App;
